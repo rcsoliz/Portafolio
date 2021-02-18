91a0efaee5692e65c4a16ab9910031806fc378e7
rcsoliz <robertoucbnh@gmail.com>
2021-02-18 00:34:52 -0400

    segunda version 18-02-21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResumenComponent } from './resumen/resumen.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+//import {APP_BASE_HREF} from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResumenComponent,
+    PortafolioComponent,
+    BlogComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
